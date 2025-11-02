5fb39b718334d5235e2e3aab36fe35abcd4f1a2a
Aguiar012 <113046681+Aguiar012@users.noreply.github.com>
2025-11-02 20:43:08 -0300

    Update ecosystem.config.cjs

--- whatsapp/ecosystem.config.cjs
+++ whatsapp/ecosystem.config.cjs
@@ -1,22 +1,27 @@
 module.exports = {
-  apps: [
-    {
-      name: "aviso_suap",
-      script: "whatsapp/aviso_suap/avisador_suap.js",
-      env: {
-        PORT: "3000",
-        WA_AUTH_DIR: "/app/data/wa_auth_suap",
-        STATE_PATH: "/app/data/state_suap.json",
-        SCORES_PATH: "/app/data/state/scores_suap.json"
-      }
-    },
-    {
-      name: "zapbot",
-      script: "whatsapp/chatbot/conversa_zap.js",
-      env: {
-        PORT: "3001",
-        WA_AUTH_DIR: "/app/data/wa_auth_zapbot"
-      }
+apps: [
+  {
+    name: "aviso_suap",
+    script: "whatsapp/aviso_suap/avisador_suap.js",
+    env: {
+      PORT: 3000,
+      DATA_DIR: "/app/data",
+      WA_AUTH_DIR: "/app/data/wa_auth_suap",
+      STATE_PATH: "/app/data/state_suap.json",
+      SCORES_PATH: "/app/data/state/scores_suap.json",
+      LOCK_FILE: "/app/data/locks/suap.lock.json"
     }
-  ]
+  },
+  {
+    name: "zapbot",
+    script: "whatsapp/chatbot/zapbot.js",
+    env: {
+      PORT: 3001,
+      DATA_DIR: "/app/data",
+      WA_AUTH_DIR: "/app/data/wa_auth_zapbot",
+      LOCK_FILE: "/app/data/locks/zapbot.lock.json"
+    }
+  }
+]
+
 };
